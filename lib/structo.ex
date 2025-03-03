8e1dcdbd7731a90f82430fbe925234f8d21b258c
Robin <robindev2019@outlook.com>
2025-03-03 16:35:31 +0100

    Add support for literal values too

--- lib/structo.ex
+++ lib/structo.ex
@@ -4,23 +4,60 @@ defmodule Structo do
 
   This library allows you to construct maps and structs like this:
 
-      ~m{a, b}
+      ~m{a, b, c: d}
 
   Instead of:
 
-      %{a: a, b: b}
+      %{a: a, b: b, c: d}
 
   And it works in matches too:
 
-      iex> ~m{a, b} = %{a: 1, b: 2}
+      iex> ~m{a, b: 2} = %{a: 1, b: 2}
       iex> a
       1
-      iex> b
-      2
 
-  ## Structs
+  """
 
-  The library works for structs too. Just `use Structo` in your module:
+  @doc false
+  def parse(expression) do
+    expression
+    |> String.split(~r/\s*,\s*/, trim: true)
+    |> Enum.map(&parse_segments/1)
+    |> Enum.uniq_by(fn {key, _} -> key end)
+  end
+
+  @doc false
+  def parse_segments(segment) do
+    case String.split(segment, ":", parts: 2) do
+      [key, value] -> {trimmed_atom(key), trimmed_quote(value)}
+      [key] -> {trimmed_atom(key), Macro.var(trimmed_atom(key), nil)}
+    end
+  end
+
+  defp trimmed_atom(s), do: s |> String.trim() |> String.to_atom()
+  defp trimmed_quote(v), do: v |> String.trim() |> Code.string_to_quoted!()
+
+  @doc """
+  This sigil acts as a shorthand for constructing maps with
+  JavaScript-like syntax.
+
+  ## Examples
+
+      iex> import Structo
+      iex> a = 1; b = 2; d = 3
+      iex> ~m{a, b, c: d}
+      %{a: 1, b: 2, c: 3}
+
+  """
+  defmacro sigil_m({:<<>>, _meta, [expr]}, []) do
+    quote do
+      %{unquote_splicing(parse(expr))}
+    end
+  end
+
+  @doc """
+  While it is a giant hack, this library can semi-work for
+  structs as well. Just `use Structo` in your module:
 
       defmodule MyStruct do
         use Structo
@@ -29,39 +66,22 @@ defmodule Structo do
 
   And then construct your structs like this:
 
-      import MyStruct
-      ~MYSTRUCT{a, b}
+      iex> import MyStruct
+      iex> a = "wibble"
+      iex> ~MYSTRUCT{a, b: "wobble"}
+      %MyStruct{a: "wibble", b: "wobble"}
 
-  (Unfortunately, the struct name has to be upcased.)
+  Unfortunately, the module name has to be upcased, due to Elixir's 
+  restrictions on sigil names.
   """
-
-  defmacro sigil_m({:<<>>, _meta, [expr]}, []) do
-    fields =
-      expr
-      |> String.split(~r/\s*,\s*/, trim: true)
-      |> Enum.map(&String.to_atom/1)
-      |> Enum.uniq()
-      |> Enum.map(&{&1, Macro.var(&1, nil)})
-
-    quote do
-      %{unquote_splicing(fields)}
-    end
-  end
-
   defmacro __using__(_opts) do
-    module = __CALLER__.module
+    mod = inspect(__CALLER__.module)
+    sigil = :"sigil_#{String.upcase(mod)}"
 
     quote do
-      defmacro unquote(:"sigil_#{String.upcase(inspect(module))}")({:<<>>, _, [expr]}, []) do
-        fields =
-          expr
-          |> String.split(~r/\s*,\s*/, trim: true)
-          |> Enum.map(&String.to_atom/1)
-          |> Enum.uniq()
-          |> Enum.map(&{&1, Macro.var(&1, nil)})
-
+      defmacro unquote(sigil)({:<<>>, _, [expr]}, []) do
         quote do
-          %unquote(__MODULE__){unquote_splicing(fields)}
+          %unquote(__MODULE__){unquote_splicing(Structo.parse(expr))}
         end
       end
     end
