a2870d57988d15b232cd02aa2b329dc39635f42c
Robin <robindev2019@outlook.com>
2025-03-14 11:22:34 +0100

    Bump version

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule Structo.MixProject do
   def project do
     [
       app: :structo,
-      version: "0.1.2",
+      version: "0.1.3",
       elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
